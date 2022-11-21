4524ff70036de5189162aab7898cc6619014122b
Charlie <charlieavery123@hotmail.co.uk>
2022-11-21 17:12:01 +0000

    adapted given clause to use API endpoint

--- .functional_tests/features/steps/given.py
+++ .functional_tests/features/steps/given.py
@@ -3,5 +3,5 @@ from behave import given
 
 @given('there are "{count}" Texts')
 def step_impl(context, count):
-    texts = Text.objects.count()
-    context.test.assertEqual(texts, count)
+    res = context.test.client.get("/texts/")
+    context.test.assertEqual(len(res.content), count)
