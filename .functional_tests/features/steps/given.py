70bf5e29845dbaad37d9c765cdbba84f32f17425
Charlie <charlieavery123@hotmail.co.uk>
2022-11-21 17:00:53 +0000

    filled out Given clause

--- .functional_tests/features/steps/given.py
+++ .functional_tests/features/steps/given.py
@@ -3,4 +3,5 @@ from behave import given
 
 @given('there are "{count}" Texts')
 def step_impl(context, count):
-    raise NotImplementedError('STEP: Given there are "0" Texts')
+    texts = Text.objects.count()
+    context.test.assertEqual(texts, count)
