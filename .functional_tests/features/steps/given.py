bd01fc21c819718707f397b10a4673b805dc2441
Charlie <charlieavery123@hotmail.co.uk>
2022-11-21 17:39:03 +0000

    changed urls to require api app

--- .functional_tests/features/steps/given.py
+++ .functional_tests/features/steps/given.py
@@ -3,5 +3,5 @@ from behave import given
 
 @given("there are no Texts")
 def step_impl(context):
-    res = context.test.client.get("/texts/")
+    res = context.test.client.get("/api/texts/")
     context.test.assertEqual(res.content, [])
