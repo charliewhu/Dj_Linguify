2aca4228ff8d4e643f2e8ac41680fd492917e29f
Charlie <charlieavery123@hotmail.co.uk>
2022-11-22 09:31:09 +0000

    added Text viewset

--- api/views.py
+++ api/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import TextSerializer
 
 # Create your views here.
+class TextViewSet(viewsets.ModelViewSet):
+    serializer_class = TextSerializer
+    queryset = Text.objects.all()
