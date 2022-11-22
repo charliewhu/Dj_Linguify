a0e20097ca2d2b1ebdc6fc82389a8a37c1b8d7b2
Charlie <charlieavery123@hotmail.co.uk>
2022-11-22 09:43:10 +0000

    created minimal Text model

--- api/views.py
+++ api/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from .serializers import TextSerializer
+from content.models import Text
 
 # Create your views here.
 class TextViewSet(viewsets.ModelViewSet):
     serializer_class = TextSerializer
-    queryset = []
+    queryset = Text.objects.all()
