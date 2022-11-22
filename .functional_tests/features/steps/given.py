f257abc5dbe81ffc975b2b62ea370f98b2904f3f
Charlie <charlieavery123@hotmail.co.uk>
2022-11-22 09:48:07 +0000

    test passing

--- .functional_tests/features/steps/given.py
+++ .functional_tests/features/steps/given.py
@@ -1,7 +1,8 @@
 from behave import given
+import json
 
 
 @given("there are no Texts")
 def step_impl(context):
     res = context.test.client.get("/api/texts/")
-    context.test.assertEqual(res.content, [])
+    context.test.assertEqual(json.loads(res.content), [])
