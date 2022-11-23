1b978a5aaa1d2de7c8ec577a3d04f8a49bd5eda4
Charlie <charlieavery123@hotmail.co.uk>
2022-11-23 11:41:15 +0000

    created viewset

--- api/views.py
+++ api/views.py
@@ -3,7 +3,12 @@ from rest_framework import viewsets
 from .serializers import TextSerializer
 from content.models import Text
 
-# Create your views here.
+
 class TextViewSet(viewsets.ModelViewSet):
+    serializer_class = TextSerializer
+    queryset = Text.objects.all()
+
+
+class WordViewSet(viewsets.ModelViewSet):
     serializer_class = TextSerializer
     queryset = Text.objects.all()
