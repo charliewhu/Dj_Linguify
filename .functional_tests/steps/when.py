c74ff1c769bc8291435c96cc2b3f2dae5a1f5121
Charlie <charlieavery123@hotmail.co.uk>
2022-11-23 13:37:39 +0000

    when passing

--- .functional_tests/steps/when.py
+++ .functional_tests/steps/when.py
@@ -13,7 +13,6 @@ def step_impl(context, name, body):
 @when('the first Word "{word}" is tagged')
 def step_impl(context, word):
     url = "/api/words/1/"
-    print({"name": str(word), "status": "Tagged"})
-    res = context.test.client.put(url, {"name": str(word), "status": "Tagged"})
+    res = context.test.client.put(url, {"name": word, "status": "Tagged"})
 
     context.test.assertEqual(res.status_code, 200)
