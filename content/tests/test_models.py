8d183afe799145a90f23cae7e2ce03472cb1eaff
Charlie <charlieavery123@hotmail.co.uk>
2022-11-23 12:27:39 +0000

    added test to create words

---
+++ content/tests/test_models.py
@@ -0,0 +1,27 @@
+from django.test import TestCase
+
+from .factory import TextFactory
+
+from ..models import Word
+
+
+class TextTest(TestCase):
+    def setUp(self):
+        self.text_body = "test text body"
+        self.text = TextFactory(body=self.text_body)
+
+    def test_words_created(self):
+        """
+        a word should be creared for each word in the Text body,
+        with status = "new"
+        """
+        self.assertEqual(Word.objects.count(), len(self.text_body.split()))
+
+    def test_duplicate_words_not_created(self):
+        """
+        if a Word already exists,
+        do not create new objects,
+        do not change status of existing object
+        """
+        # add new text with word crossover
+        pass
