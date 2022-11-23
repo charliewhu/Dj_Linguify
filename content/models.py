7a8e1eb29601cccff4e12501da7b1d37aa1e429f
Charlie <charlieavery123@hotmail.co.uk>
2022-11-23 11:56:27 +0000

    created Word model

--- content/models.py
+++ content/models.py
@@ -5,3 +5,8 @@ from django.db import models
 class Text(models.Model):
     name = models.CharField(max_length=50, null=True)
     body = models.TextField(null=True)
+
+
+class Word(models.Model):
+    name = models.CharField(max_length=50)
+    status = models.CharField(max_length=10)
