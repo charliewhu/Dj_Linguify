f4d5b41104a94907247e177d7eef30911b93604d
Charlie <charlieavery123@hotmail.co.uk>
2022-11-28 16:10:23 +0000

    refactored response data

--- .functional_tests/steps/then.py
+++ .functional_tests/steps/then.py
@@ -5,41 +5,36 @@ import json
 @then('there are "{count}" Texts')
 def step_impl(context, count):
     res = context.test.client.get("/api/texts/")
-    obj = json.loads(res.content)
 
-    context.test.assertEqual(len(obj), int(count))
+    context.test.assertEqual(len(res.data), int(count))
 
 
 @then('there are "{count}" Words')
 def step_impl(context, count):
     res = context.test.client.get("/api/words/")
-    obj = json.loads(res.content)
 
-    context.test.assertEqual(len(obj), int(count))
+    context.test.assertEqual(len(res.data), int(count))
 
 
 @then('the Text has body "{body}"')
 def step_impl(context, body):
     res = context.test.client.get("/api/texts/")
-    obj = json.loads(res.content)
-    print(obj)
-    context.test.assertEqual(obj[0].get("body"), body)
+
+    context.test.assertEqual(res.data[0].get("body"), body)
 
 
 @then('the Text will have name "{name}"')
 def step_impl(context, name):
     res = context.test.client.get("/api/texts/")
-    obj = json.loads(res.content)
-    context.test.assertEqual(obj[0].get("name"), name)
+    context.test.assertEqual(res.data[0].get("name"), name)
 
 
 @then('the Word will have status = "{status}"')
 def step_impl(context, status):
     url = "/api/words/1/"
     res = context.test.client.get(url)
-    obj = json.loads(res.content)
 
-    context.test.assertEqual(obj.get("status"), status)
+    context.test.assertEqual(res.data.get("status"), status)
 
 
 @then('the Texts related Words have "{attr}" attribute')
