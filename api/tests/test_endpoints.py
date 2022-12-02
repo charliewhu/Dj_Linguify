573dd17bcea7e1b4ceca94f278f1cee38e80d04e
Charlie <charlieavery123@hotmail.co.uk>
2022-12-02 08:44:37 +0000

    added endpoint test for TextWordDetail

--- api/tests/test_endpoints.py
+++ api/tests/test_endpoints.py
@@ -19,3 +19,24 @@ class TextDetailTest(APITestCase):
                 "body": self.text.body,
             },
         )
+
+
+class TextWordDetailTest(APITestCase):
+    def setUp(self):
+        self.url = "/api/text_words/1/"
+        self.body = "test"
+        self.text = TextFactory(body=self.body)
+
+    def test_GET(self):
+        res = self.client.get(self.url)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(
+            res.json(),
+            [
+                {
+                    "id": 1,
+                    "text": self.text.id,
+                    "word": self.body,
+                },
+            ],
+        )
