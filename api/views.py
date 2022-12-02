26bb6dba4adaa859002b668581bedf39f5eeed4d
Charlie <charlieavery123@hotmail.co.uk>
2022-12-02 08:18:58 +0000

    overwrite retrieve and queryset to filter on TextWord.text id

--- api/views.py
+++ api/views.py
@@ -19,4 +19,13 @@ class WordViewSet(viewsets.ModelViewSet):
 
 class TextWordViewSet(viewsets.ModelViewSet):
     serializer_class = TextWordSerializer
-    queryset = TextWord.objects.all()
+
+    def get_queryset(self, *args, **kwargs):
+        text_id = self.kwargs.get("pk")
+        text_words = TextWord.objects.filter(text=text_id)
+        return text_words
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_queryset()
+        serializer = self.get_serializer(instance, many=True)
+        return Response(serializer.data)
